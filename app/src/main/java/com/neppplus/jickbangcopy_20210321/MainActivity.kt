eb7ccb13db6b3c5c2c178b99156faeacf5fd42eb
전은형 <ooooo5h91@gmail.com>
2021-03-21 17:24:24 +0900

    Room 클래스를 직렬화 (Serializable)해서 통째로 첨부 가능하도록.

--- app/src/main/java/com/neppplus/jickbangcopy_20210321/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210321/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.jickbangcopy_20210321
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20210321.adapters.RoomAdapter
@@ -30,6 +31,16 @@ class MainActivity : AppCompatActivity() {
         mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRooms)
 
         roomListView.adapter = mRoomAdapter
+
+        roomListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedRoom = mRooms[position]
+
+            val myIntent = Intent(this, ViewRoomDetailActivity::class.java)
+            myIntent.putExtra("roomInfo", clickedRoom)
+            startActivity(myIntent)
+
+        }
 
     }
 }
--- app/src/main/java/com/neppplus/jickbangcopy_20210321/datas/Room.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210321/datas/Room.kt
@@ -1,5 +1,6 @@
 package com.neppplus.jickbangcopy_20210321.datas
 
+import java.io.Serializable
 import java.text.NumberFormat
 import java.util.*
 
@@ -7,7 +8,7 @@ class Room(
     val price : Int,
     val address : String,
     val floor : Int,
-    val description : String) {
+    val description : String) : Serializable {
 
     fun getFormattedPrice() : String {
 
